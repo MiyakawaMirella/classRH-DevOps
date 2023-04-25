2a2762e577d6d9a2f6163104fbff49cd4f8f5922
MiyakawaMirella <mimimiyakawa@gmail.com>
2023-04-25 16:47:14 -0300

    subindo os arquivos

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<RH> listaFuncionario = new ArrayList();
+        RH funcionario1 = new RH("Mirella", "234567890", "11985663143", new Date(), new Date(), "noite");
+        RH funcionario2 = new RH("Lucas", "324565432", "40028922", new Date(), new Date(), "Manhã");
+        RH funcionario3 = new RH("João", "2345665433456", "999999900", new Date(), new Date(), "Tarde");
+
+        listaFuncionario.add(funcionario1);
+        listaFuncionario.add(funcionario2);
+        listaFuncionario.add(funcionario3);
+    }
+}
---
+++ src/RH.java
@@ -0,0 +1,63 @@
+import java.util.Date;
+
+public class RH {
+    String nome;
+    String cpf;
+    String telefone;
+    Date nascimento;
+    Date agendamento;
+    String período;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public Date getAgendamento() {
+        return agendamento;
+    }
+
+    public void setAgendamento(Date agendamento) {
+        this.agendamento = agendamento;
+    }
+
+    public String getPeríodo() {
+        return período;
+    }
+
+    public void setPeríodo(String período) {
+        this.período = período;
+    }
+
+    public RH(String nome, String cpf, String telefone, Date nascimento, Date agendamento, String período) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.nascimento = nascimento;
+        this.agendamento = agendamento;
+        this.período = período;
+    }
+}
